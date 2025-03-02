761075affffe589805b53650bdab76de345d6158
MaryMachuma <machhmary173@gmail.com>
2025-03-02 10:33:07 +0300

    Updated SQL files for bears lab with passing tests

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+    ('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', 1),
+    ('Rowdy', 3, 'M', 'Black', 'Playful', 1),
+    ('Tabitha', 4, 'F', 'Brown', 'Calm', 1),
+    ('Sergeant Brown', 6, 'M', 'Brown', 'Grumpy', 1),
+    ('Melissa', 2, 'F', 'Black', 'Shy', 0),
+    ('Grinch', 7, 'M', 'Gray', 'Aggressive', 1),
+    ('Wendy', 3, 'F', 'Brown', 'Gentle', 1),
+    (NULL, 4, 'F', 'White', 'Quiet', 1);
